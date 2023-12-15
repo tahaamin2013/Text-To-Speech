84d688abeaf478dca6a0b216574d06adf0202c99
tahaamin2013 <tahaamindob2013@gmail.com>
2023-12-16 02:07:36 +0500

    fafdas

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
 const nextConfig = {}
 
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    sw: 'service-worker.js',
+  },
+  // Other Next.js configurations...
+});
+
 module.exports = nextConfig
+
